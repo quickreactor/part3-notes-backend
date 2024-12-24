b3260d76d7add74ea217f25903afa2548f86482f
Barnaby Fredric <36432141+quickreactor@users.noreply.github.com>
2024-12-24 16:28:52 +1300

    moved frontend dist in

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const cors = require("cors");
 
 app.use(cors());
 app.use(express.json());
+app.use(express.static('dist'))
 
 let notes = [
     {
@@ -77,6 +78,7 @@ app.get("/api/notes/:id", (request, response) => {
         response.status(404).end();
     }
 });
+
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
