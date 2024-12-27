125aa60d89a2c5558532104bfb05e0364afa58be
Barnaby Fredric <36432141+quickreactor@users.noreply.github.com>
2024-12-27 16:48:15 +1300

    finished changing backend to use mongoDB

--- index.js
+++ index.js
@@ -1,41 +1,34 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+require("dotenv").config();
+const Note = require("./models/note");
 
 app.use(cors());
 app.use(express.json());
-app.use(express.static('dist'))
+app.use(express.static("dist"));
 
-let notes = [
-    {
-        id: 1,
-        content: "HTML is easy",
-        date: "2022-01-10T17:30:31.098Z",
-        important: true,
-    },
-    {
-        id: 2,
-        content: "Browser can execute only Javascript",
-        date: "2022-01-10T18:39:34.091Z",
-        important: false,
-    },
-    {
-        id: 3,
-        content: "GET and POST are the most important methods of HTTP protocol",
-        date: "2022-01-10T19:20:14.298Z",
-        important: true,
-    },
-];
-
-app.get("/", (req, res) => {
-    res.send("<h1>Hello World!</h1>");
+// GET ALL NOTES
+app.get("/api/notes", (req, res) => {
+    Note.find({}).then((notes) => {
+        res.json(notes);
+    });
 });
 
-const generateId = () => {
-    const maxId = notes.length > 0 ? Math.max(...notes.map((n) => n.id)) : 0;
-    return maxId + 1;
-};
+// GET NOTE BY ID
+app.get("/api/notes/:id", (request, response) => {
+    Note.findById(request.params.id)
+        .then((note) => {
+            if (note) {
+                response.json(note);
+            } else {
+                response.status(404).end();
+            }
+        })
+        .catch((err) => next(err));
+});
 
+// CREATE NEW NOTE
 app.post("/api/notes", (request, response) => {
     const body = request.body;
 
@@ -45,42 +38,51 @@ app.post("/api/notes", (request, response) => {
         });
     }
 
-    const note = {
+    const note = new Note({
         content: body.content,
         important: body.important || false,
-        date: new Date(),
-        id: generateId(),
+    });
+
+    note.save().then((savedNote) => {
+        response.json(savedNote);
+    });
+});
+
+// CHANGE NOTE BY ID
+app.put("/api/notes/:id", (req, res) => {
+    const { content, important } = req.body;
+
+    const note = {
+        content,
+        important,
     };
 
-    notes = notes.concat(note);
-
-    response.json(note);
+    Note.findByIdAndUpdate(req.params.id, note, { new: true })
+        .then((updatedNote) => res.json(updatedNote))
+        .catch((err) => next(err));
 });
 
-app.get("/api/notes", (req, res) => {
-    res.json(notes);
-});
-
+//DELETE NOTE BY ID
 app.delete("/api/notes/:id", (request, response) => {
-    const id = Number(request.params.id);
-    notes = notes.filter((note) => note.id !== id);
-
-    response.status(204).end();
+    Note.findByIdAndDelete(request.params.id)
+        .then((result) => response.status(204).end())
+        .catch((err) => next(err));
 });
 
-app.get("/api/notes/:id", (request, response) => {
-    const id = Number(request.params.id);
-    const note = notes.find((note) => note.id === id);
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message);
 
-    if (note) {
-        response.json(note);
-    } else {
-        response.status(404).end();
+    if (error.name === "CastError") {
+        return response.status(400).send({ error: "malformatted id" });
     }
-});
 
+    next(error);
+};
 
-const PORT = process.env.PORT || 3001
+// this has to be the last loaded middleware, also all the routes should be registered before this!
+app.use(errorHandler);
+
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
-})
+    console.log(`Server running on port ${PORT}`);
+});
---
+++ models/note.js
@@ -0,0 +1,35 @@
+// MOGOOSE STUFF ------------------------------------------
+const mongoose = require("mongoose");
+const util = require("util");
+require("dotenv").config();
+
+const url = process.env.MONGODB_URI;
+
+mongoose.set("strictQuery", false);
+// console.log(process.env, "lol")
+
+mongoose
+    .connect(url)
+    .then((res) => {
+        console.log(`Connected to MongoDB...`);
+    })
+    .catch((err) => {
+        console.log(`Error connecting to MongoDB: ${err}`);
+    });
+
+const noteSchema = new mongoose.Schema({
+    content: String,
+    important: Boolean,
+});
+
+noteSchema.set("toJSON", {
+    transform: (document, returnedObject) => {
+        returnedObject.id = returnedObject._id.toString();
+        delete returnedObject._id;
+        delete returnedObject.__v;
+    },
+});
+
+module.exports = mongoose.model("Note", noteSchema);
+
+// ENDF MONGOOSE ---------------------------------------
